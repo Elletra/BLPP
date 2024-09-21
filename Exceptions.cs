c9a09dd44a603f4859f904750021d56c31e72056
Elletra <elletra@proton.me>
2024-09-20 21:17:44 -0700

    Cleanup exceptions

--- Exceptions.cs
+++ Exceptions.cs
@@ -12,72 +12,31 @@ using BLPP.Preprocessor;
 
 namespace BLPP
 {
-	public class UnexpectedTokenException : Exception
+	public class UnexpectedTokenException(string token, int line) : Exception($"Unexpected token `{token}` on line {line}")
 	{
-		public UnexpectedTokenException() { }
-		public UnexpectedTokenException(string token, int line) : base($"Unexpected token `{token}` on line {line}") { }
 		public UnexpectedTokenException(char ch, int line) : this($"{ch}", line) { }
 		public UnexpectedTokenException(Token token) : this(token.Value, token.Line) { }
-		public UnexpectedTokenException(string message) : base(message) { }
-		public UnexpectedTokenException(string message, Exception inner) : base(message, inner) { }
 	}
 
-	public class UnexpectedEndOfLineException : Exception
+	public class UnexpectedEndOfLineException(int line) : Exception($"Unexpected end of line on line {line}")
 	{
-		public UnexpectedEndOfLineException() { }
-		public UnexpectedEndOfLineException(int line) : base($"Unexpected end of line on line {line}") { }
 		public UnexpectedEndOfLineException(Token token) : this(token.Line) { }
-		public UnexpectedEndOfLineException(string message) : base(message) { }
-		public UnexpectedEndOfLineException(string message, Exception inner) : base(message, inner) { }
 	}
 
-	public class UnexpectedEndOfCodeException : Exception
+	public class UnexpectedEndOfCodeException(int line) : Exception($"Unexpected end of code on line {line}")
 	{
-		public UnexpectedEndOfCodeException() { }
-		public UnexpectedEndOfCodeException(int line) : base($"Unexpected end of code on line {line}") { }
 		public UnexpectedEndOfCodeException(Token token) : this(token.Line) { }
-		public UnexpectedEndOfCodeException(string message) : base(message) { }
-		public UnexpectedEndOfCodeException(string message, Exception inner) : base(message, inner) { }
 	}
 
-	public class UnterminatedStringException : Exception
-	{
-		public UnterminatedStringException() { }
-		public UnterminatedStringException(int line) : base($"Unterminated string at line {line}") { }
-		public UnterminatedStringException(string message) : base(message) { }
-		public UnterminatedStringException(string message, Exception inner) : base(message, inner) { }
-	}
+	public class UnterminatedStringException(int line) : Exception($"Unterminated string at line {line}") { }
+	public class UnterminatedCommentException(int line) : Exception($"Unterminated comment at line {line}") { }
 
-	public class UnterminatedCommentException : Exception
+	public class SyntaxException(string message, int line) : Exception($"Syntax error on line {line}: {message}")
 	{
-		public UnterminatedCommentException() { }
-		public UnterminatedCommentException(int line) : base($"Unterminated comment at line {line}") { }
-		public UnterminatedCommentException(string message) : base(message) { }
-		public UnterminatedCommentException(string message, Exception inner) : base(message, inner) { }
-	}
-
-	public class SyntaxException : Exception
-	{
-		public SyntaxException() { }
-		public SyntaxException(string message, int line) : base($"Syntax error on line {line}: {message}") { }
 		public SyntaxException(string message, Token token) : this(message, token.Line) { }
-		public SyntaxException(string message) : base(message) { }
-		public SyntaxException(string message, Exception inner) : base(message, inner) { }
 	}
 
-	public class UndefinedMacroException : Exception
-	{
-		public UndefinedMacroException() { }
-		public UndefinedMacroException(Token token) : base($"Undefined macro `{token.Value}` on line {token.Line}") { }
-		public UndefinedMacroException(string message) : base(message) { }
-		public UndefinedMacroException(string message, Exception inner) : base(message, inner) { }
-	}
-
-	public class UndefinedMacroParameterException : Exception
-	{
-		public UndefinedMacroParameterException() { }
-		public UndefinedMacroParameterException(Token token) : base($"Undefined macro parameter `{token.Value}` on line {token.Line}") { }
-		public UndefinedMacroParameterException(string message) : base(message) { }
-		public UndefinedMacroParameterException(string message, Exception inner) : base(message, inner) { }
-	}
+	public class UndefinedMacroException(Token token) : Exception($"Undefined macro `{token.Value[1..]}` on line {token.Line}") { }
+	public class UndefinedMacroParameterException(Token token) : Exception($"Undefined macro parameter `{token.Value[2..]}` on line {token.Line}") { }
+	public class MultipleDefinitionsException(Token token) : Exception($"Multiple definitions for `{token.Value}` macro (line {token.Line})") { }
 }
