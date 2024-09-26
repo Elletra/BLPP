0604e0d84c79f907616260c38cd4a435b7225d0a
Elletra <elletra@proton.me>
2024-09-25 23:03:52 -0700

    Some exceptions now take an optional `col` argument

--- Shared/Exceptions.cs
+++ Shared/Exceptions.cs
@@ -2,14 +2,22 @@
 {
 	public class FileExtensionException(string file, string ext) : Exception($"File \"{file}\" does not have a '{ext}' extension") { }
 
-	public class UnexpectedTokenException(int line, string token) : Exception($"Unexpected token `{token}` on line {line}")
+	public class UnexpectedTokenException : Exception
 	{
+		public UnexpectedTokenException(int line, string token) : base($"Unexpected token `{token}` on line {line}") { }
 		public UnexpectedTokenException(int line, char ch) : this(line, $"{ch}") { }
+		public UnexpectedTokenException(int line, int col, string token) : base($"Unexpected token `{token}` on line {line}, col {col}") { }
+		public UnexpectedTokenException(int line, int col, char ch) : this(line, col, $"{ch}") { }
 	}
 
 	public class UnexpectedEndOfLineException(int line) : Exception($"Unexpected end of line on line {line}") { }
 	public class UnexpectedEndOfCodeException(int line) : Exception($"Unexpected end of code on line {line}") { }
-	public class UnterminatedStringException(int line) : Exception($"Unterminated string at line {line}") { }
-	public class UnterminatedCommentException(int line) : Exception($"Unterminated comment at line {line}") { }
-	public class SyntaxException(int line, string message) : Exception($"Syntax error on line {line}: {message}") { }
+	public class UnterminatedStringException(int line) : Exception($"Unterminated string on line {line}") { }
+	public class UnterminatedCommentException(int line) : Exception($"Unterminated comment on line {line}") { }
+
+	public class SyntaxException : Exception
+	{
+		public SyntaxException(int line, string message) : base($"Syntax error on line {line}: {message}") { }
+		public SyntaxException(int line, int col, string message) : base($"Syntax error on line {line}, col {col}: {message}") { }
+	}
 }
