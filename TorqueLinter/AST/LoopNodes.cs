0f2274d0610f1d1516bc495579c39ba70aef30b2
Elletra <elletra@proton.me>
2024-09-28 14:58:36 -0700

    Commit `LoopNodes.cs`

---
+++ TorqueLinter/AST/LoopNodes.cs
@@ -0,0 +1,20 @@
+﻿/**
+ * LoopNodes.cs
+ *
+ * Copyright (C) 2024 Elletra
+ *
+ * This file is part of the TorqueLinter source code. It may be used under the BSD 3-Clause License.
+ *
+ * For full terms, see the LICENSE file or visit https://spdx.org/licenses/BSD-3-Clause.html
+ */
+
+using TorqueLinter.Lexer;
+
+namespace TorqueLinter.AST
+{
+	public class WhileLoopNode(Token startToken, Node test) : Node(startToken)
+	{
+		public readonly Node Test = test;
+		public List<Node> Body { get; set; } = [];
+	}
+}
